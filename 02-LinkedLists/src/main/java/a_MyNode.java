ddc42645a7347b0f7efebd1b2b3cb188a0a62510
Zeynep <zeynepbstg@hotmail.com>
2025-09-06 11:00:10 -0400

    a_node implemented

---
+++ 02-LinkedLists/src/main/java/a_MyNode.java
@@ -0,0 +1,8 @@
+public class a_MyNode {
+    String name;
+    a_MyNode next; // linking node objects to each other
+
+    public a_MyNode(String name) {
+        this.name = name;
+    }
+}
---
+++ 02-LinkedLists/src/main/java/a_NodeExampleApp.java
@@ -0,0 +1,24 @@
+public class a_NodeExampleApp {
+
+    public static void main(String[] args) {
+        a_MyNode nodeA = new a_MyNode("Node A");
+        a_MyNode nodeB = new a_MyNode("Node B");
+        a_MyNode nodeC = new a_MyNode("Node C");
+
+        System.out.println(nodeA);  // it will rint the address of the node! NOT the NAME!
+
+        nodeA.next = nodeB;
+        nodeB.next = nodeC;
+        System.out.println(nodeA.name + " " + nodeA.next.next + " " + nodeB.next.name);
+
+        // Iterate over the list i did
+        a_MyNode current = nodeA;
+        while(current != null){
+            System.out.println(current.name);
+            current = current.next;
+        }
+
+
+    }
+
+}
