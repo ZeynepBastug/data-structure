98443bc9c4be84f968767d7c299ccb3833a33f86
Zeynep <zeynepbstg@hotmail.com>
2025-09-06 10:43:02 -0400

    three sum drcond dolution with hashmap added

--- 01-Arrays/src/main/java/ThreeSum.java
+++ 01-Arrays/src/main/java/ThreeSum.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class ThreeSum {
 
@@ -41,9 +39,37 @@ public class ThreeSum {
                 }
             }
         }
-
         return triplets;
 
     }
 
+    // HashMap solution for 3 Sum problem
+    // Overall complexity is: nlogn + n + O(n^2)
+    public static List<List<Integer>> threeNumSumHashMap (int[] array, int target){
+
+        Set<List<Integer>> result = new HashSet<>();
+        HashMap <Integer, Integer> map = new HashMap<>();
+
+        Arrays.sort(array); // O(nlogn)
+
+        // Put all numbers in  a HashMap to create a look up pointer
+        for (int i = 0; i < array.length; i++) {
+            map.put(array[i], i);   //O(n)
+        }
+
+        // in a nested loop try to find a match for each pair
+        // from hashmap that ads up to target
+        for (int i = 0; i < array.length-1; i++) {
+            for (int j = i+1 ; j < array.length ; j++) {
+                List<Integer> triplets = new ArrayList<>();
+                int potentialMatcher = target - array[i] - array[j];
+
+                if(map.containsKey(potentialMatcher) && map.get(potentialMatcher) > j){
+                    triplets.add(array[i]);
+                    triplets.add(array[j]);
+                    triplets.add(potentialMatcher);
+                    result.add(triplets);
+                }}}
+        return new ArrayList<>(result);
+    }
 }
